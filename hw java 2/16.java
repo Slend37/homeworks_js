e79f019403adbaa46e6bcc182a16649074292c6c
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2024-09-16 15:09:26 +0300

    Update 16.java

--- hw java 2/16.java
+++ hw java 2/16.java
@@ -7,11 +7,7 @@ public class Main
         System.out.println("Enter number: ");
         int k = reader.nextInt();
         reader.close();
-        
-        if (k%3 == 0  || k%5 == 0){
-            System.out.println("YES");   
-        }else{
-            System.out.println("NO");
+        // не решил :( 
         }
     }
 }
