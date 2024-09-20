afc86893da0447d15e4721bbca94507221e7d9f6
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2024-09-20 13:46:35 +0300

    Update 16.java

--- hw java 2/16.java
+++ hw java 2/16.java
@@ -7,7 +7,21 @@ public class Main
         System.out.println("Enter number: ");
         int k = reader.nextInt();
         reader.close();
-        // не решил :( 
+        int c = k;
+        while ( c%5 != 0 && c%3 != 0 && c>0){
+            c-=3;
         }
+        if (c<0){
+            c=k;
+            while (c%5 != 0 && c%3 != 0 && c>0){
+                c-=5;
+            }if (c<0){
+            System.out.println(k +" НЕТ");
+            }else{
+                System.out.println(k +" ДА");
+            }
+        }else{
+            System.out.println(k + " ДА");
+            }
     }
 }
