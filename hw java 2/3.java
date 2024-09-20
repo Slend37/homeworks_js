bae529c6dee0858ee67878ff476af54e77eee45b
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2024-09-20 13:52:31 +0300

    Update 3.java

--- hw java 2/3.java
+++ hw java 2/3.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      int c = 32;
-      System.out.println(c +" по цельсию, ");
-      System.out.println(32+c*9/5 +" по фаренгейту");
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Введите градусы по цельсию: ");
+        int c = reader.nextInt();
+        reader.close();
+        float t = c;
+        System.out.println(t +" по цельсию, ");
+        System.out.println(32.0+t*9/5 +" по фаренгейту");
     }
 }
