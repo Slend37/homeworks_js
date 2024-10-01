fbbaaac7ad8f9616bf5b69bd3e52ff1f871d47d5
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2024-10-01 13:21:47 +0300

    Delete hw java 2 directory
    moving hw java 2 to the new repository for Java

--- hw java 2/1.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-      float n = 8.9f;
-      System.out.println(Math.round(n));
-    }
-}
--- hw java 2/5.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-      for (int i=25; i<63;i++){
-          if (i%2==0){
-              System.out.println(i);
-          }
-      }
-    }
-}
